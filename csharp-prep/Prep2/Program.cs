c6dfc858ffaf62033c9dd8190f0be1c66f219261
Bosenc1122 <Carter.Bosen99@gmail.com>
2023-01-06 16:48:30 -0700

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string Input = Console.ReadLine();
+        int grade = int.Parse(Input);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "Your grade is an A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "Your grade is a B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "Your grade is a C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "Your grade is a D";
+        }
+        else
+        {
+            letter = "Your grade is an F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
